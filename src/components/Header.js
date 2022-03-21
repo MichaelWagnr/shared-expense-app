72f19a99cf5fc0d992d52b711560d691ed35d1a1
MichaelWagnr <michaelwagner780@gmail.com>
2022-03-21 19:46:35 -0400

    Add container div to App component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Royal Mint</h1>
+            <button>Start</button>
+        </header>
+    )
+}
+
+
+export default Header;
