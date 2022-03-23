5b3cda0c0ece6050df928cc6e38bc6e3dbbabb3a
MichaelWagnr <michaelwagner780@gmail.com>
2022-03-22 22:27:01 -0400

    Remove bootstrap dependency

---
+++ src/components/Form.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Form = () => {
+    return (
+        <div>I'm a form</div>
+    )
+}
+
+export default Form;
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,6 @@ const Header = () => {
     return (
         <header>
             <h1>Royal Mint</h1>
-            <button>Start</button>
         </header>
     )
 }
