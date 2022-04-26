6d552809eaee682348da33e5ac4138a279a79489
MichaelWagnr <michaelwagner780@gmail.com>
2022-04-26 17:15:06 -0400

    Add form template

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 const Form = () => {
     return (
-        <div>...</div>
+        <div className="form">
+            <p>Submit an expense / shared purchase:</p>
+            <form>
+                <label htmlFor="purchaser">Purchased by:</label>
+                <select name="purchaser" id="purchaser">
+                    <option value="A">A</option>
+                    <option value="B">B</option>
+                </select>
+                <label htmlFor="total">Receipt total:</label>
+                <input name="total" type="text" />
+                {/* <label htmlFor="shared">Percentage shared:</label>
+                <input type="range" min="0" max="100" value="50" name="shared" id="total-shared" /> */}
+                <br />
+                <button type="submit">Submit</button>
+
+            </form>
+        </div>
     )
 }
 
