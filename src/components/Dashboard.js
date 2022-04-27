335d55510d2cc2a7af0f99c61a4d8619ad6710d9
MichaelWagnr <michaelwagner780@gmail.com>
2022-04-27 15:02:49 -0400

    Add History and Dashboard components

---
+++ src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div className="dashboard">
+            Totals ...
+        </div>
+    );
+}
+
+export default Dashboard;
