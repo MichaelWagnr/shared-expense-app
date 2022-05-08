80b7621d305dd3cac63f6d58dc4ec799d4329dd0
MichaelWagnr <michaelwagner780@gmail.com>
2022-05-08 16:38:49 -0400

    Add delete button, nonfunctional

--- src/components/Purchase.js
+++ src/components/Purchase.js
@@ -6,6 +6,7 @@ const Purchase = (props) => {
             <span className="purchaser">{props.purchaser}</span>
             <span className="total">{props.total}</span>
             <span className="description">{props.desc}</span>
+            <button className="delete">&times;</button>
         </div>
     )
 }
