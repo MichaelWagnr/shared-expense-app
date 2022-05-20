fe17a33851685e68b6b3af42c0cf2edb14f16df6
MichaelWagnr <michaelwagner780@gmail.com>
2022-05-20 17:38:27 -0400

    Render debt totals as stylable p tags

--- src/components/Totals.js
+++ src/components/Totals.js
@@ -1,15 +1,6 @@
 import React from 'react';
 
 const Totals = ({ submissions, nameA, nameB }) => {
-    // //Seperate purchases by purchaser
-    // const aPurchases = submissions.filter(submissions => submissions.purchaser === nameA);
-    // const bPurchases = submissions.filter(submissions => submissions.purchaser === nameB);
-
-    // //Total purchase amounts for other person
-    // const a4b = aPurchases
-    //     .map(purchase => {
-    //         return (purchase.total * (purchase.shared / 100))
-    //     }).reduce((a, b) => { return (a + b) }, 0);
 
     const totalOwedTo = (name, submissions) => {
         return submissions
@@ -24,13 +15,15 @@ const Totals = ({ submissions, nameA, nameB }) => {
     const aOwesB = totalOwedTo(nameB, submissions);
 
     return (
-        <div className="total">
-            {
-                bOwesA > aOwesB
-                    ? `${nameB} owes ${nameA} ${bOwesA - aOwesB}`
-                    : `${nameA} owes ${nameB} ${aOwesB - bOwesA}`
-            }
-        </div>
+        bOwesA > aOwesB
+            ? <div className="total">
+                <p className="debter">{nameB} owes {nameA}:</p>
+                <p className="debt">{bOwesA - aOwesB}</p>
+            </div>
+            : <div className="total">
+                <p className="debter">{nameA} owes {nameB}:</p>
+                <p className="debt">{aOwesB - bOwesA}</p>
+            </div>
     );
 }
 
